22bffe7bb16149e4630cc7f59bcdb34fb9986223
chad <changda0616@gmail.com>
2020-01-13 16:34:31 +0800

    初始化表單元件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
+import { ModelDrivenFormComponent } from './model-driven-form/model-driven-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateDrivenFormComponent,
+    ModelDrivenFormComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/model-driven-form/model-driven-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-model-driven-form',
+  templateUrl: './model-driven-form.component.html',
+  styleUrls: ['./model-driven-form.component.scss']
+})
+export class ModelDrivenFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
